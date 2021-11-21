191ac967afa0d9a5ff9682a9d9184b0ac7a5c7b2
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2021-11-21 23:58:12 +0100

    Day 9 2020.

---
+++ 2020/day9.jl
@@ -0,0 +1,38 @@
+function part1(input, preamble_length_str = "25")
+    preamble_length = parse(Int, preamble_length_str)
+    numbers = parse.(Int, readlines(input))
+    for i = (preamble_length + 1):length(numbers)
+        if !is_sum_of_two(numbers[i], numbers[(i - preamble_length):(i - 1)])
+            return numbers[i]
+        end
+    end
+    @assert false
+end
+
+function part2(input, preamble_length_str = "25")
+    preamble_length = parse(Int, preamble_length_str)
+    numbers = parse.(Int, readlines(input))
+    invalid = part1(input, preamble_length_str)
+    for i = 1:length(numbers)
+        partial_sum = numbers[i]
+        j = i + 1
+        while j <= length(numbers)
+            partial_sum += numbers[j]
+            if partial_sum == invalid
+                return sum(extrema(numbers[i:j]))
+            elseif partial_sum > invalid
+                break
+            end
+            j += 1
+        end
+    end
+end
+
+function is_sum_of_two(n, v)
+    for i = 1:length(v)
+        for j = (i + 1):length(v)
+            v[i] + v[j] == n && return true
+        end
+    end
+    return false
+end
--- aoc.jl
+++ aoc.jl
@@ -1,6 +1,6 @@
 using Downloads: download
 
-2 <= length(ARGS) <= 3 || error("Usage: julia aoc.jl YEAR DAY [TEST]")
+length(ARGS) >= 2 || error("Usage: julia aoc.jl YEAR DAY [TEST] [EXTRA_ARGS]")
 year, day = ARGS[1:2]
 test = length(ARGS) > 2
 input_file = joinpath(@__DIR__, year, "input", "day$(day)")
@@ -19,11 +19,11 @@ if isdefined(Main, :part1)
     print("Part 1: ")
     test && print(ARGS[3])
     println()
-    println(part1(input_file))
+    println(part1(input_file, ARGS[4:end]...))
 end
 if isdefined(Main, :part2)
     print("Part 2: ")
     test && print(ARGS[3])
     println()
-    println(part2(input_file))
+    println(part2(input_file, ARGS[4:end]...))
 end
