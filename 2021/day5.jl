b5dcfd0df7c8e5e91676e98d7a63fb25ff60956f
Gunnar Farnebäck <gunnar.farneback@inify.com>
2022-11-27 14:59:34 +0100

    2021 day 5: Switch from dict to array.

--- 2021/day5.jl
+++ 2021/day5.jl
@@ -1,28 +1,30 @@
 function part1(input)
     lines = parse_input(input)
     filter!(line -> line[1] == line[3] || line[2] == line[4], lines)
-    map = place_lines(lines)
-    return count(>(1), values(map))
+    return place_lines(lines)
 end
 
 function part2(input)
     lines = parse_input(input)
-    map = place_lines(lines)
-    return count(>(1), values(map))
+    return place_lines(lines)
 end
 
 parse_input(input) = parse_line.(readlines(input))
-parse_line(line) =  parse.(Int, split(replace(line, " -> " => ","), ","))
+parse_line(line) = parse.(Int, split(replace(line, " -> " => ","), ","))
 
 function place_lines(lines)
-    map = Dict{Tuple{Int, Int}, Int}()
+    map = zeros(Int, 1000, 1000)
+    vents = 0
     for (x1, y1, x2, y2) in lines
         Δx = sign(x2 - x1)
         Δy = sign(y2 - y1)
         for i = 0:max(abs(x2 - x1), abs(y2 - y1))
-            coord = (x1 + i * Δx, y1 + i * Δy)
-            map[coord] = get(map, coord, 0) + 1
+            x, y = x1 + i * Δx, y1 + i * Δy
+            map[y, x] += 1
+            if map[y, x] == 2
+                vents += 1
+            end
         end
     end
-    return map
+    return vents
 end
